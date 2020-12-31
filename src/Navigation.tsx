42b7c1fa51e2506efd8054c3c574381aa491b24b
Arca <arca.artem@gmail.com>
2020-12-31 15:56:41 +0000

    :recycle: simplify navigation code

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { 
-  Pane,
+  Card,
   TabNavigation,
   Tab
 } from 'evergreen-ui';
@@ -10,36 +10,27 @@ export default function Navigation() {
   const [selectedTab, setSelectedTab] = useState('home');
 
   const routes = [
-    { key: 'home', to: '/', text: 'Home' },
-    { key: 'stars', to: '/stars', text: 'Stars' },
-    { key: 'advanced-table', to: '/advanced-table', text: 'Advanced Table' },
-    { key: 'somewhere', to: '/somewhere', text: 'Somewhere new' },
+    { key: 'home', path: '/', text: 'Home' },
+    { key: 'stars', path: '/stars', text: 'Stars' },
+    { key: 'advanced-table', path: '/advanced-table', text: 'Advanced Table' },
+    { key: 'somewhere', path: '/somewhere', text: 'Somewhere new' },
 
   ];
 
   const navLinks = routes.map(route => {
     return (
-        <NavLink to={route.to} isActive={(match, _location) => {
-          if (match) {
-            setSelectedTab(route.key);
-            return true;
-          }
-
-          return false;
-        }}>
-          <Tab key={route.key} id={route.key} isSelected={selectedTab === route.key}>
-            {route.text}
-          </Tab>
-        </NavLink>
+      <Tab key={route.key} id={route.key} isSelected={selectedTab === route.key} onSelect={() => setSelectedTab(route.key)}>
+        <Link to={route.path}>{route.text}</Link>
+      </Tab>
     );
   });
 
   return (
-    <Pane background="blueTint" borderRadius={4}>
+    <Card background="blueTint">
       <TabNavigation>
         {navLinks}
       </TabNavigation>
-    </Pane>
+    </Card>
   );
 }
 
