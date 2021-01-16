2cf0b1c5b266e79efd80ea28fe84b602ee38fee5
Arca <arca.artem@gmail.com>
2021-01-16 21:16:56 +0000

    :rotating_light: add missing commas semicolons

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,39 @@
 import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import url from 'url';
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     }
   })
 
-  win.loadFile('dist/index.html')
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+
+  if (process.env.NODE_ENV === "development") {
+    win.webContents.openDevTools();
+  }
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
-})
+});
